84f1aafcafc73abdca5ede80c3f22e173c5a9244
Gáspár Róbert fejlesztő Felhasználói Rendszerfejlesztői Osztály <Robert.Gaspar@groupama.hu>
2018-05-24 08:21:07 +0200

    changes made, version changed to 1.1.0-SNAPSHOT

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("Hello world!");
         System.out.println("Changed to develop branch, bumped version");
+        System.out.println("Made some changes, bumped version to 1.1.0-SNAPSHOT");
     }
 
 }
